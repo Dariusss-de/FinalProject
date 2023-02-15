61749e0b3a9a7f11212d1d155c69c8ae7c107ca5
Dmitry <mangosovdima@mail.ru>
2023-02-15 21:11:37 +0300

    added task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+void NaturalТumber(int N)
+{
+    int counter=1;
+   if (N < 0) Console.Write($"{N} не натуральное число");
+    if(N !=0)
+{
+      
+       Console.Write(N) ;
+        NaturalТumber(N-1);
+}          
+}
+
+Console.WriteLine("Введите число N: ");
+int Number = int.Parse(Console.ReadLine());
+NaturalТumber(Number);
