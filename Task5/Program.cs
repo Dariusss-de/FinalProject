1801e1f85b5fdccebd71f1a9ff8c572d351850b9
Dmitry <mangosovdima@mail.ru>
2023-02-16 22:12:13 +0300

    added task5

---
+++ Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму цифр числа
+
+int SumNum(int arg)
+{
+    if(arg<10) return arg;
+
+    int digit = arg %10;
+    int nextValue = arg/10;
+     return digit+SumNum(nextValue);
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(SumNum(number));
